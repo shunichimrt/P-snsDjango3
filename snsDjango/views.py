4122805e96557dbd0a204f6583636e1ddc556bfe
shunichimrt <smurata.official@gmail.com>
2022-05-11 15:30:58 +0900

    tabelogmok-withoutAPI

--- snsDjango/views.py
+++ snsDjango/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect  # redirectを追加
 
-# Create your views here.
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'snsDjango/index.html'
